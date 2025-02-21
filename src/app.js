7f643aafbb44317be71ec611b86d5518c1dd6b87
CoderRedwing <ajiteshru@gmail.com>
2025-02-21 14:36:10 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -11,5 +11,9 @@ app.use(express.json());
 app.use("/api", router);
 app.use(errorHandler);
 
+app.get("/", (req, res) => {
+    res.send("API is running!");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
